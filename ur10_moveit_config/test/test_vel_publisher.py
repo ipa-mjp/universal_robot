3a476c46a0dbe70254c6fb9227ef25c7f62c531a
ipa-mjp <myp@ipa.fhg.de>
2020-06-11 09:46:55 +0200

    added velocity control to ur10

---
+++ ur10_moveit_config/test/test_vel_publisher.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+import rospy
+from std_msgs.msg import Float64MultiArray
+
+
+if __name__ == "__main__":
+	rospy.init_node('add_collisions_py')
+        rate = rospy.Rate(10) # 10hz
+
+	pub = rospy.Publisher("/arm/joint_group_velocity_controller/command", Float64MultiArray, queue_size = 1000)
+	rospy.sleep(1.0)
+
+	pub_data = Float64MultiArray()
+	for i in range(0, 10):
+		pub_data.data = [-1.0,-1.0,-1.0,-1.0,-1.0,-1.0]
+		pub.publish(pub_data)
+		rospy.sleep(0.05)
+	for i in range(0, 10):
+		pub_data.data = [0.0,0.0,0.0,0.0,0.0,0.0]
+		pub.publish(pub_data)
+		rospy.sleep(0.05)
+	
+
